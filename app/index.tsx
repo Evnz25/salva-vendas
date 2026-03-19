d59bf95069da23bade39ab19b6cde04670d31c43
Evnz25 <evandronicolau55@gmail.com>
2026-03-18 23:34:53 -0300

    update

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Salva-vendas</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
