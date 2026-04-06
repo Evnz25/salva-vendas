b94c035dd0cdb0ff7cf5629a2d27e4e19d84421b
Evnz25 <evandronicolau55@gmail.com>
2026-04-06 09:46:56 -0300

    desenvolvimento da tela de clientes

--- components/ultimosClientes.tsx
+++ components/ultimosClientes.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import Svg, { Path } from "react-native-svg";
+import ClientInformation from "./ui/clientInformation";
 
 export default function UltimosClientes() {
   return (
@@ -8,31 +8,7 @@ export default function UltimosClientes() {
         <Text style={style.font_title}>Últimos Clientes Cadastrados</Text>
         <Text style={style.font_also}>Ver todos {`>`}</Text>
       </View>
-      <View style={style.container_client}>
-        <View style={style.container_clientInitials}>
-          <Text style={style.font_clientInitials}>CH</Text>
-        </View>
-        <View>
-          <Text style={style.font_clientName}>Carlos Henrique</Text>
-          <Text style={style.font_clientEmail}>carloshenrique@example.com</Text>
-        </View>
-        <View style={style.container_svg}>
-          <Svg
-            width={15}
-            height={15}
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke={"#0F2B5B"}
-          >
-            <Path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
-            />
-          </Svg>
-        </View>
-      </View>
+      <ClientInformation />
     </View>
   );
 }
